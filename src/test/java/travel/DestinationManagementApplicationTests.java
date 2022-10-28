99b3ee2a1ca0d643d509fbea17361b06f2edcad4
hong9Lee <hongtemp9mac@gmail.com>
2022-10-28 16:09:44 +0900

    first commit

---
+++ src/test/java/travel/DestinationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
